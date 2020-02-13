ff81bd53689b82e3995afa94934f34dfce14820d
gamindaishara <gamindaishara@users.noreply.github.com>
2020-02-13 16:45:02 +0530

    edit agricultural specialist

--- src/htmlBlocks/AgriculturalSpecialistHTML.java
+++ src/htmlBlocks/AgriculturalSpecialistHTML.java
@@ -25,5 +25,33 @@ public class AgriculturalSpecialistHTML {
 		
 		return text;
 	}
-
+	
+	public static String displayQuestions(String title, String body, String askedTime) {
+		String conciseBody = body.substring(0, 250);
+		String text = 
+				"                <div class=\"row\">\r\n" + 
+						"                    <div class=\"col-md-12\">\r\n" + 
+						"                        <div class=\"card\">\r\n" + 
+						"                            <div class=\"header\">\r\n" + 
+						"                                <h4 class=\"title\">"+ title + "</h4>\r\n" + 
+						"                            </div>\r\n" + 
+						"                            <div class=\"content\">\r\n" + 
+						"								<p class=\"category\">"+ conciseBody + "</p>\r\n" + 
+						"								<br>\r\n" + 
+						"                                <div class=\"footer\">\r\n" + 
+						"                                    <div class=\"stats\">\r\n" + 
+						"                                        <i class=\"fa fa-clock-o\"></i>" + askedTime + "\r\n" + 
+						"                                    </div>\r\n" + 
+						"                                </div>\r\n" + 
+						"                            </div>\r\n" + 
+						"                        </div>\r\n" + 
+						"					</div>\r\n" + 
+						"				</div>\r\n";;
+						
+		return text;
+	}
+	
+	public static String displayFullQuestion(String title, String body, String askedTime, String asker, String region) {
+		return null;
+	}
 }
