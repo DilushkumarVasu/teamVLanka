b234fda60b924e6a45513408583de0e5499b498d
gamindaishara <57451056+gamindaishara@users.noreply.github.com>
2020-02-16 08:42:54 +0530

    Update AgriculturalSpecialistRegistration.java

--- src/agriculturalSpecialist/AgriculturalSpecialistRegistration.java
+++ src/agriculturalSpecialist/AgriculturalSpecialistRegistration.java
@@ -38,7 +38,7 @@ public class AgriculturalSpecialistRegistration extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 		
 		String name = request.getParameter("name");
